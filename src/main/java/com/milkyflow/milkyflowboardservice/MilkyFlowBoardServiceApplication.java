22acf8eee77b5c3e0b584c521708429885e6e0bc
Gizelly Steffanny <gizellysteffanny@gmail.com>
2023-10-26 18:28:55 -0300

    feat: first commit

---
+++ src/main/java/com/milkyflow/milkyflowboardservice/MilkyFlowBoardServiceApplication.java
@@ -0,0 +1,13 @@
+package com.milkyflow.milkyflowboardservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MilkyFlowBoardServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MilkyFlowBoardServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/milkyflow/milkyflowboardservice/MilkyFlowBoardServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.milkyflow.milkyflowboardservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MilkyFlowBoardServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
